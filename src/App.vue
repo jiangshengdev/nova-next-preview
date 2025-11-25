fc7eca6f0a66a10ae65eaf1a9e5735fb08ae4edd
Jiang Sheng <jiangshengdev@outlook.com>
2025-11-25 13:19:07 +0800

    pnpm create vue

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,85 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div>{{ state.color }}</div>
-  <NovaColorPicker
-    :preset="preset"
-    :value="state.color"
-    @update="onUpdate"
-    alpha
-  ></NovaColorPicker>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<script>
-import { reactive } from 'vue'
-import { NovaColorPicker } from '@jiangshengdev/nova-next'
-import '@jiangshengdev/nova-next/dist/css/themes.css'
-import '@jiangshengdev/nova-next/dist/css/dropdown.css'
-import '@jiangshengdev/nova-next/dist/css/color-picker.css'
-
-const preset = [
-  '#dc143c',
-  '#f0e68c',
-  '#8fbc8f',
-  '#00ced1',
-  '#1e90ff',
-  '#663399',
-  '#708090',
-]
-
-export default {
-  components: {
-    NovaColorPicker,
-  },
-  setup() {
-    const state = reactive({
-      color: '#663399',
-    })
-
-    function onUpdate(color) {
-      state.color = color
-    }
-
-    return {
-      state,
-      onUpdate,
-      preset,
-    }
-  },
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
-</script>
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
